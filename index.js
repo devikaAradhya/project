ad0edc8500ce0fcda457eeee030ac8a36d6bfb39
devikaAradhya <dd9660709@gmail.com>
2023-12-04 18:56:41 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,12 @@
+// 
+// const googleDatabase=[{gmail:"test@gmail.com",owner:"test",password:"1234"},{gmail:"simple@gmail.com",owner:"simple",password:"123"}]
+// const len=googleDatabase.length;
+// for(let i=0;i<len;i++){
+//     console.log(googleDatabase[i].owner)
+// };
+const obj={
+    name:"test",
+}
+
+console.error("hello");
+console.log("hello");
